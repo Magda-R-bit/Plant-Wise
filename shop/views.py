0f30af805cf80bf7fde3850371bc97f827868fbb
Magda <magda.redka@gmail.com>
2025-04-23 16:03:17 +0100

    Created views and urls for shop app, added products images

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.all()
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    return render(request, 'shop/products.html', {
+        'category': category,
+        'categories': categories,
+        'products': products
+    })
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    return render(request, 'shop/product_detail.html', {'product': product})
