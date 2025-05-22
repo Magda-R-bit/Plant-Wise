a7c3d5d8c9875d45377d68079bf1fdfc7b46b739
Magda <magda.redka@gmail.com>
2025-05-22 23:16:47 +0100

    Updated cart.html with authentication and completed webhook handler

--- checkout/urls.py
+++ checkout/urls.py
@@ -13,5 +13,14 @@ urlpatterns = [
         views.checkout_success,
         name='checkout_success'
     ),
-    path('wh/', webhook, name='webhook'),
+    path(
+        'cache_checkout_data/',
+        views.cache_checkout_data,
+        name='cache_checkout_data'
+    ),
+    path(
+        'wh/',
+        webhook,
+        name='webhook'
+    ),
 ]
