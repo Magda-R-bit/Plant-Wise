06ec2648ae59161009f3de7aef70a7182254e379
Magda <magda.redka@gmail.com>
2025-06-12 02:45:03 +0100

    Fixed too long lines

--- cart/urls.py
+++ cart/urls.py
@@ -2,8 +2,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_cart, name='view_cart'),
-    path('add/<slug:slug>/', views.add_to_cart, name='add_to_cart'),
-    path('update/<slug:slug>/', views.update_cart, name='update_cart'),
-    path('remove/<slug:slug>/', views.remove_from_cart, name='remove_from_cart'),
+    path(
+        '',
+        views.view_cart,
+        name='view_cart'
+    ),
+    path(
+        'add/<slug:slug>/',
+        views.add_to_cart,
+        name='add_to_cart'
+    ),
+    path(
+        'update/<slug:slug>/',
+        views.update_cart,
+        name='update_cart'
+    ),
+    path(
+        'remove/<slug:slug>/',
+        views.remove_from_cart,
+        name='remove_from_cart'
+    ),
 ]
--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.wishlist_view, name='wishlist'),
-    path('add/<slug:product_slug>/', views.add_to_wishlist, name='add_to_wishlist'),
-    path('remove/<slug:product_slug>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+    path(
+        '',
+        views.wishlist_view,
+        name='wishlist'
+    ),
+    path(
+        'add/<slug:product_slug>/',
+        views.add_to_wishlist,
+        name='add_to_wishlist'
+    ),
+    path(
+        'remove/<slug:product_slug>/',
+        views.remove_from_wishlist,
+        name='remove_from_wishlist'
+    ),
 ]
